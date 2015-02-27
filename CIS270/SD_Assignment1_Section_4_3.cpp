873270cfd71773e16c2214533bea131950bbfa54
Rev Obrera <rev.obrera@gmail.com>
2015-02-27 12:11:38 -0500

    Section4.3 | Problem 3

--- CIS270/SD_Assignment1_Section_4_3.cpp
+++ CIS270/SD_Assignment1_Section_4_3.cpp
@@ -29,68 +29,101 @@
 //	return 0;
 //}
 
+///* 
+//=================
+//Program 4.5 | #2
+//=================
+//*/
+////2a)
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	char code;
+//	int weight;
+//	int bin;
+//	
+//	cout << "Enter a letter grade: ";
+//	cin >> grade;
+//	
+//	cout << "Enter weight number: ";
+//	cin >> weight;
+//	
+//	if (grade == 'A') || (grade == 'a')
+//		if (weight > 35)
+//			bin = 1
+//		else
+//			//Do nothing...
+//	//Code will execute t = s + a
+//	else
+//		//Do nothing...
+//
+//	return 0;
+//}
+//
+////2b)
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int grade;
+//	int sum;
+//	int count;
+//	int fail;
+//	
+//	cout << "Enter a number grade: ";
+//	cin >> grade;
+//	
+//	cout << "Enter a number count: ";
+//	cin >> count;
+//	
+//	fail = 0;
+//	sum = 0;
+//	if (count < 5)
+//		if (grade < 50)
+//			fail = fail + 1;
+//		else
+//			//Do nothing...
+//	else
+//		//Do nothing...
+//	
+//	
+//	return 0;
+//}
+
 /* 
 =================
-Program 4.5 | #2
+Program 4.5 | #3
 =================
 */
-//2a)
+//3)
+
 
 #include <iostream>
 using namespace std;
 
-int main() {
-	char code;
-	int weight;
-	int bin;
+int main()
+{
 	
-	cout << "Enter a letter grade: ";
-	cin >> grade;
+	int digout;
+	double inlbs;
 	
-	cout << "Enter weight number: ";
-	cin >> weight;
+	cout << "Enter the input weight: ";
+	cin >> inlbs;
 	
-	if (grade == 'A') || (grade == 'a')
-		if (weight > 35)
-			bin = 1
-		else
-			//Do nothing...
-	//Code will execute t = s + a
-	else
-		//Do nothing...
-
-	return 0;
-}
-
-//2b)
-
-#include <iostream>
-using namespace std;
-
-int main() {
-	int grade;
-	int sum;
-	int count;
-	int fail;
+	if (inlbs >= 90) digout = 1111;
+	if ((inlbs >= 80) && (inlbs <= 90))
+		digout = 1110;
+	if ((inlbs >= 70) && (inlbs <= 80)) 
+		digout = 1101;
+	if ((inlbs >= 60) && (inlbs >= 70)) 
+		digout = 1100;
+	if (inlbs < 1000) digout = 1011;
 	
-	cout << "Enter a number grade: ";
-	cin >> grade;
-	
-	cout << "Enter a number count: ";
-	cin >> count;
-	
-	fail = 0;
-	sum = 0;
-	if (count < 5)
-		if (grade < 50)
-			fail = fail + 1;
-		else
-			//Do nothing...
-	else
-		//Do nothing...
-	
+	cout << "The digital output is " << digout << endl;
 	
 	return 0;
 }
-
 
